d26966ae90bb8c5ace6fd4b018cf2dd2661c8308
sssofi0101 <55346176+sssofi0101@users.noreply.github.com>
2022-08-16 14:19:13 +0300

    Реализовала логику диалога для выхода из аккаунта

--- app/src/main/java/com/example/universitysystem/LogoutFragment.kt
+++ app/src/main/java/com/example/universitysystem/LogoutFragment.kt
@@ -2,11 +2,16 @@ package com.example.universitysystem
 
 
 
+import android.content.Context
+import android.content.SharedPreferences
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.FrameLayout
 import androidx.core.content.ContentProviderCompat.requireContext
+import androidx.navigation.fragment.findNavController
 import com.google.android.material.bottomsheet.BottomSheetBehavior
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 
@@ -14,12 +19,27 @@ import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 
 class LogoutFragment : BottomSheetDialogFragment() {
 
-
+    private val checkSettings = "check_settings"
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? = inflater.inflate(R.layout.fragment_logout, container, false)
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        view.findViewById<Button>(R.id.bs_logoutButton).setOnClickListener {
+            val sharedPref: SharedPreferences? = activity?.getPreferences(Context.MODE_PRIVATE)
+            sharedPref?.edit()?.putBoolean(checkSettings, false)?.apply()
+            findNavController().navigate(R.id.loginFragment)
+            dismiss()
+        }
+        view.findViewById<Button>(R.id.bs_cancelButton).setOnClickListener {
+            view.findViewById<FrameLayout>(R.id.standard_bottom_sheet).apply {
+                dismiss()
+            }
+        }
+    }
 
     companion object {
         const val TAG = "ModalBottomSheet"
