4dd3d6bca642d226bedcce7d95668326f2f5d1dc
Stepan Nemchenko <54963637+BadLog1n@users.noreply.github.com>
2022-09-08 19:10:50 +0300

    Реализовано отображение расписания

--- app/src/main/java/com/example/universitysystem/ScheduleFragment.kt
+++ app/src/main/java/com/example/universitysystem/ScheduleFragment.kt
@@ -1,45 +1,128 @@
 package com.example.universitysystem
 
+import android.annotation.SuppressLint
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.View
-import android.widget.ArrayAdapter
-import android.widget.Spinner
-import android.widget.TextView
+import android.widget.*
+import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.universitysystem.databinding.FragmentScheduleBinding
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
 import com.xwray.groupie.GroupAdapter
 import com.xwray.groupie.GroupieViewHolder
 import com.xwray.groupie.Item
 
 
 class ScheduleFragment : Fragment(R.layout.fragment_schedule) {
+    private lateinit var database: DatabaseReference
 
 
     private lateinit var binding:FragmentScheduleBinding
 
 
+    @SuppressLint("UseSwitchCompatOrMaterialCode")
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        val spinner = requireView().findViewById<Spinner>(R.id.spinner)
+        val switch = requireView().findViewById<Switch>(R.id.switchh)
+
         super.onViewCreated(view, savedInstanceState)
         binding = FragmentScheduleBinding.inflate(layoutInflater)
         activity?.findViewById<androidx.appcompat.widget.Toolbar>(R.id.toolbar1)?.title = "Расписание"
-        var groups: Array<String> = arrayOf("ПО-91б","ПО-92б")
-        val spinner = view.findViewById<Spinner>(R.id.spinner)
-        var arrayAdapter:ArrayAdapter<String> = ArrayAdapter(this.requireContext(),android.R.layout.simple_spinner_item,groups)
-        arrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
-        spinner.adapter = arrayAdapter
-
+        getGroups(spinner)
         val adapter = GroupAdapter<GroupieViewHolder>()
         val scheduleRc: RecyclerView = view.findViewById(R.id.scheduleRc)
-        adapter.add(WeekDayItem("Понедельник"))
-        adapter.add(ScheduleRecordItem("8.00","Базы данных(лб)","а-207, Аникина Е.И."))
-        adapter.add(WeekDayItem("Понедельник"))
-        adapter.add(ScheduleRecordItem("8.00","Базы данных(лб)","а-207, Аникина Е.И."))
+        //timetableGet(view.findViewById<Spinner>(R.id.spinner).selectedItem.toString(), "up")
         scheduleRc.adapter = adapter
         scheduleRc.layoutManager = LinearLayoutManager(this.context)
+        //timetableGet("П")
+
+
+        spinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener{
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+
+            }
+            override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+                val switchState: Boolean = switch.isChecked
+                timetableGet(spinner.selectedItem.toString(), switchState)
+            }
+        }
+
+
+        switch.setOnCheckedChangeListener(CompoundButton.OnCheckedChangeListener { buttonView, isChecked ->
+            val switchState: Boolean = switch.isChecked
+            timetableGet(spinner.selectedItem.toString(), switchState)
+        })
+
+
+
+
+
+
     }
+
+
+
+    private fun getGroups(spinner: Spinner) {
+        val groups: ArrayList<String> = arrayListOf()
+        database = FirebaseDatabase.getInstance().getReference("timetable")
+        val requestToDatabase = database.get()
+        requestToDatabase.addOnSuccessListener {
+            for (item in it.children){
+                groups.add(item.key.toString())
+            }
+            val arrayAdapter:ArrayAdapter<String> = ArrayAdapter(this.requireContext(),android.R.layout.simple_spinner_item,groups)
+            arrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            spinner.adapter = arrayAdapter
+
+        }
+    }
+
+    private var subjects = arrayListOf<ScheduleRecordItem>()
+    private fun timetableGet(group: String, upDown: Boolean) {
+        val adapter = GroupAdapter<GroupieViewHolder>()
+        val scheduleRc: RecyclerView = requireView().findViewById(R.id.scheduleRc)
+        var upDownText = "up"
+        if (upDown) upDownText = "down"
+
+        val rusDay = arrayOf("Понедельник", "Вторник", "Среда","Четверг","Пятница","Суббота")
+        val day = arrayOf("monday", "tuesday", "wednesday", "thursday", "friday", "saturday")
+        database = FirebaseDatabase.getInstance().getReference("timetable/$group/$upDownText")
+        val requestToDatabase = database.get()
+        requestToDatabase.addOnSuccessListener { gets ->
+            day.forEachIndexed { index, element ->
+                for (timeCabSub in gets.child(element).children) {
+                    val subject =
+                        ScheduleRecordItem(
+                            timeCabSub.key.toString(),
+                            timeCabSub.child("cabName").value.toString(),
+                            timeCabSub.child("subject").value.toString()
+                        )
+                    subjects.add(subject)
+                }
+                subjects = java.util.ArrayList(subjects.sortedWith(compareBy { it.time }))
+                adapter.add(WeekDayItem(rusDay[index]))
+                for (item in subjects) {
+                    adapter.add(ScheduleRecordItem(item.time,  item.cabName, item.subject))
+                }
+                subjects.clear()
+            }
+            scheduleRc.adapter = adapter
+            scheduleRc.layoutManager = LinearLayoutManager(this.context)
+        }
+
+
+    }
+
 }
+
+
+
+
+
+
+
 class WeekDayItem(val text: String) : Item<GroupieViewHolder>() {
     override fun bind(viewHolder: GroupieViewHolder, position: Int) {
         viewHolder.itemView.findViewById<TextView>(R.id.weekday_tv).text = text
