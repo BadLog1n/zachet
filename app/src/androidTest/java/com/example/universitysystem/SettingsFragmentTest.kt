6fdb806f7e599d54f158ad1bad567acf592ec8dd
sssofi0101 <55346176+sssofi0101@users.noreply.github.com>
2022-10-29 19:56:29 +0300

    Подчистила код, убрала ненужное

--- app/src/androidTest/java/com/example/universitysystem/SettingsFragmentTest.kt
+++ app/src/androidTest/java/com/example/universitysystem/SettingsFragmentTest.kt
@@ -27,14 +27,7 @@ class SettingsFragmentTest {
     private lateinit var mdbref:DatabaseReference
     @Before
     fun setUp() {
-
-
         mun = "someUn"
-        //val marr:Array<String?> = arrayOf("name","surname","login","password")
-        //mnet = EditText(Mockito.mock(Context::class.java))
-       // mset = EditText(Mockito.mock(Context::class.java))
-        //mlet = EditText(Mockito.mock(Context::class.java))
-       // mpet = EditText(Mockito.mock(Context::class.java))
         scenario = launchFragmentInContainer<SettingsFragment>()
         try {
             Thread.sleep(2000)
@@ -47,33 +40,15 @@ class SettingsFragmentTest {
             mnet = this.view?.findViewById<EditText>(R.id.nameText)!!
             mset = this.view?.findViewById<EditText>(R.id.surnameText)!!
         }
-        /*mnet = Mockito.mock(EditText::class.java)
-        mnet.text = Mockito.mock(Editable::class.java)
-        mnet.setText("")
-        mset= Mockito.mock(EditText::class.java)
-        mlet= Mockito.mock(EditText::class.java)
-        mpet= Mockito.mock(EditText::class.java)*/
         mdb = Mockito.mock(MyDatabase::class.java)
        mdbref = Mockito.mock(DatabaseReference::class.java)
-        Mockito.`when`(mdb.getSettingsInfo(mdbref,mun,mnet,mset,mlet,mpet)).thenAnswer(Answer { invocation ->
-        /*Mockito.`when`(mdb.getSettingsInfo(mun,mnet,mset,mlet,mpet)).thenAnswer(Answer { invocation ->
-            invocation.getArgument<EditText>(1).setText("name")
-            invocation.getArgument<EditText>(2).setText("surname")
-            invocation.getArgument<EditText>(3).setText("login")
-            invocation.getArgument<EditText>(4).setText("password")*/
+       Mockito.`when`(mdb.getSettingsInfo(mdbref,mun,mnet,mset,mlet,mpet)).thenAnswer(Answer { invocation ->
             mset.setText("surname")
             mpet.setText("password")
             mnet.setText("name")
             mlet.setText("login")
         })
-            /*.thenAnswer(it ->
-            mset.setText("surname")
-            mpet.setText("password")
-            mnet.setText("name")
-            mlet.setText("login")
-        ))*/
-       fr.getDbInfo(mdbref,mdb,mun,mnet,mset,mlet,mpet)
-       // fr.getDbInfo(mdb,mun,mnet,mset,mlet,mpet)
+       //fr.getDbInfo(mdbref,mdb,mun,mnet,mset,mlet,mpet)
     }
     @Test
     fun changePassword(){
@@ -86,44 +61,13 @@ class SettingsFragmentTest {
 
     @Test
     fun getDbInfo() {
-        /*val mdb = Mockito.mock(MyDatabase::class.java)
-        val mun:String = "someUn"
-        //val marr:Array<String?> = arrayOf("name","surname","login","password")
-        val mnet:EditText = Mockito.mock(EditText::class.java)
-        val mset:EditText = Mockito.mock(EditText::class.java)
-        val mlet:EditText = Mockito.mock(EditText::class.java)
-        val mpet:EditText = Mockito.mock(EditText::class.java)
-        Mockito.`when`(mdb.getSettingsInfo(mun,mnet,mset,mlet,mpet)).then {
-            mset.setText("surname")
-            mpet.setText("password")
-            mnet.setText("name")
-            mlet.setText("login")
-        }
-        fr.getDbInfo(mdb,mun,mnet,mset,mlet,mpet)*/
-       /* mset.setText("surname")
-        mpet.setText("password")
-        mnet.setText("name")
-        mlet.setText("login")*/
-        /*Mockito.`when`(mdb.getSettingsInfo(mun,mnet,mset,mlet,mpet)).then {
-            mset.setText("surname")
-            mpet.setText("password")
-            mnet.setText("name")
-            mlet.setText("login")
-        }
-        fr.getDbInfo(mdb,mun,mnet,mset,mlet,mpet)*/
         Mockito.`when`(mdb.getSettingsInfo(mdbref,mun,mnet,mset,mlet,mpet)).thenAnswer(Answer { invocation ->
-        //Mockito.`when`(mdb.getSettingsInfo(mun,mnet,mset,mlet,mpet)).thenAnswer(Answer { invocation ->
-            /*invocation.getArgument<EditText>(1).setText("name")
-            invocation.getArgument<EditText>(2).setText("surname")
-            invocation.getArgument<EditText>(3).setText("login")
-            invocation.getArgument<EditText>(4).setText("password")*/
             mset.setText("surname")
             mpet.setText("password")
             mnet.setText("name")
             mlet.setText("login")
         })
        fr.getDbInfo(mdbref,mdb,mun,mnet,mset,mlet,mpet)
-       // fr.getDbInfo(mdb,mun,mnet,mset,mlet,mpet)
         try {
             Thread.sleep(5000)
         } catch (e: InterruptedException) {
--- app/src/main/java/com/example/universitysystem/MyDatabase.kt
+++ app/src/main/java/com/example/universitysystem/MyDatabase.kt
@@ -12,38 +12,19 @@ open class MyDatabase (){
     //open public fun getSettingsInfo(path:String, nameET: EditText, surnameET: EditText, loginET: EditText, passwordET: EditText) {
         //val db= FirebaseDatabase.getInstance().getReference(path)
         val requestToDatabase = db.get()
-        //val arr =mutableListOf<String>()
-        //var arr : Array<String?>
-       // return requestToDatabase
-       // if ()
-        //var success:Boolean = false
-        /*if (requestToDatabase.isSuccessful){
-            requestListener(requestToDatabase,arr)
-        }*/
+
        requestToDatabase.addOnSuccessListener {
-            //success = true
-            //arr = arrayOf("","","","")
             if (it.child("name").value.toString() != "null")
             {nameET.setText(it.child("name").value.toString())}
             else nameET.setText("")
             if (it.child("surname").value.toString() != "null") surnameET.setText(it.child("surname").value.toString()) else  surnameET.setText("")
             loginET.setText(it.child("login").value.toString())
             passwordET.setText(it.child("password").value.toString())
-            //return arr
         }
-        //val array:Array<String?> = arrayOf(arr[0],arr[1],arr[2],arr[3]) // индекс за границами массива
-         //return array
-        /*if (!success ){
-            arr = arrayOf("","","","")
-            return arr
-        }
-        return arr*/
     }
 
     private fun requestListener(requestToDatabase: Task<DataSnapshot>, arr: Array<String>) {
         requestToDatabase.addOnSuccessListener {
-            //success = true
-            //arr = arrayOf("","","","")
             if (it.child("name").value.toString() != "null") {
                 arr[0] = it.child("name").value.toString()
             } else arr[0] = ""
@@ -53,17 +34,4 @@ open class MyDatabase (){
             arr[3] = it.child("password").value.toString()
         }
     }
-    /*private fun successListenerResult(requestToDatabase: Task<DataSnapshot>) : Array<String?>{
-        requestToDatabase.addOnSuccessListener {
-
-            val arr:Array<String> = arrayOf("","","","")
-            if (it.child("name").value.toString() != "null")
-            {arr[0] = it.child("name").value.toString()}
-            else arr[0]=""
-            if (it.child("surname").value.toString() != "null") arr[1]=it.child("surname").value.toString() else  arr[1]=""
-            arr[2]=it.child("login").value.toString()
-            arr[3]= it.child("password").value.toString()
-            return arr
-        }
-    }*/
 }
