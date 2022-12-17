27d747e049e63eab14f995a33e0c55f45b2829be
Stepan Nemchenko <54963637+BadLog1n@users.noreply.github.com>
2022-12-17 10:34:02 +0300

    Логин первые 6 символов uid + какая-то буква
    в регистрации

--- app/src/main/java/com/oneseed/zachet/RegistrationFragment.kt
+++ app/src/main/java/com/oneseed/zachet/RegistrationFragment.kt
@@ -24,7 +24,6 @@ class RegistrationFragment : Fragment() {
         activity?.findViewById<DrawerLayout>(R.id.drawer)
             ?.setDrawerLockMode(DrawerLayout.LOCK_MODE_LOCKED_CLOSED)
         val regEmailText = view.findViewById<EditText>(R.id.regEmailText)
-        val regLoginText = view.findViewById<EditText>(R.id.regLoginText)
         val regPasswordText = view.findViewById<EditText>(R.id.regPasswordText)
         val regCodeText = view.findViewById<EditText>(R.id.regCodeText)
         auth = Firebase.auth
@@ -53,79 +52,62 @@ class RegistrationFragment : Fragment() {
                 return@setOnClickListener
             }
 
-            if (regLoginText.text.toString().isEmpty()) {
-                Toast.makeText(
-                    requireContext(),
-                    "Логин не может быть пустым",
-                    Toast.LENGTH_SHORT
-                ).show()
-                return@setOnClickListener
-            } else {
-                var isTeacher: Boolean
-                val databaseRef = FirebaseDatabase.getInstance().getReference("login")
-                    .child(regLoginText.text.toString()).get()
-                databaseRef.addOnSuccessListener { item ->
-                    if (item.exists()) {
-                        Toast.makeText(
-                            requireContext(),
-                            "Логин уже занят",
-                            Toast.LENGTH_SHORT
-                        ).show()
-                        return@addOnSuccessListener
-                    }
-                    val databaseRefNew = FirebaseDatabase.getInstance().getReference("code")
-                        .child(regCodeText.text.toString()).get()
-                    databaseRefNew.addOnSuccessListener { element ->
-                        if (!element.exists()) {
+
+            var isTeacher: Boolean
+            val databaseRefNew = FirebaseDatabase.getInstance().getReference("code")
+                .child(regCodeText.text.toString()).get()
+            databaseRefNew.addOnSuccessListener { element ->
+                if (!element.exists()) {
+                    Toast.makeText(
+                        requireContext(),
+                        "Код не подходит",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                    return@addOnSuccessListener
+                } else {
+                    isTeacher = element.value.toString().toBoolean()
+                }
+                auth.createUserWithEmailAndPassword(
+                    regEmailText.text.toString(),
+                    regPasswordText.text.toString()
+                )
+                    .addOnCompleteListener(requireActivity()) { task ->
+                        if (task.isSuccessful) {
+                            val uid = Firebase.auth.currentUser?.uid
+
+                            val userLogin = uid.toString().lowercase().take(6) + ("a..z").random()
+                            
+                            FirebaseDatabase.getInstance().getReference("login")
+                                .child(userLogin).setValue(uid)
+                            FirebaseDatabase.getInstance()
+                                .getReference("users/${uid.toString()}/login")
+                                .setValue(userLogin)
+                            FirebaseDatabase.getInstance().getReference("code")
+                                .child(regCodeText.text.toString()).removeValue()
+                            // Sign in success, update UI with the signed-in user's information
                             Toast.makeText(
                                 requireContext(),
-                                "Код не подходит",
+                                "Успешная регистрация",
                                 Toast.LENGTH_SHORT
                             ).show()
-                            return@addOnSuccessListener
-                        } else {
-                            isTeacher = element.value.toString().toBoolean()
-                        }
-                        auth.createUserWithEmailAndPassword(
-                            regEmailText.text.toString(),
-                            regPasswordText.text.toString()
-                        )
-                            .addOnCompleteListener(requireActivity()) { task ->
-                                if (task.isSuccessful) {
-                                    val uid = Firebase.auth.currentUser?.uid
-                                    FirebaseDatabase.getInstance().getReference("login")
-                                        .child(regLoginText.text.toString()).setValue(uid)
-                                    FirebaseDatabase.getInstance()
-                                        .getReference("users/${uid.toString()}/login")
-                                        .setValue(regLoginText.text.toString())
-                                    FirebaseDatabase.getInstance().getReference("code")
-                                        .child(regCodeText.text.toString()).removeValue()
-                                    // Sign in success, update UI with the signed-in user's information
-                                    Toast.makeText(
-                                        requireContext(),
-                                        "Успешная регистрация",
-                                        Toast.LENGTH_SHORT
-                                    ).show()
-                                    if (isTeacher) {
-                                        FirebaseDatabase.getInstance()
-                                            .getReference("users/${uid.toString()}/isTeacher")
-                                            .setValue(true)
-                                    }
-                                    findNavController().navigate(R.id.loginFragment)
-
-
-                                } else {
-                                    // If sign in fails, display a message to the user.
-                                    Toast.makeText(
-                                        requireContext(),
-                                        "Пожалуйста, проверьте вводимые данные",
-                                        Toast.LENGTH_SHORT
-                                    ).show()
-                                }
+                            if (isTeacher) {
+                                FirebaseDatabase.getInstance()
+                                    .getReference("users/${uid.toString()}/isTeacher")
+                                    .setValue(true)
                             }
+                            findNavController().navigate(R.id.loginFragment)
 
+
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Toast.makeText(
+                                requireContext(),
+                                "Пожалуйста, проверьте вводимые данные",
+                                Toast.LENGTH_SHORT
+                            ).show()
+                        }
                     }
-                }
+
 
             }
 
