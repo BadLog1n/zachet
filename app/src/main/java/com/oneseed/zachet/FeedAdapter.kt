c41b6c72dbb76ed5317e5dd95541b3e9cbd2eebc
Stepan Nemchenko <54963637+BadLog1n@users.noreply.github.com>
2023-01-04 09:08:01 +0300

    Убраны лишние импорты

--- app/src/main/java/com/oneseed/zachet/FeedAdapter.kt
+++ app/src/main/java/com/oneseed/zachet/FeedAdapter.kt
@@ -1,16 +1,12 @@
 package com.oneseed.zachet
 
-import android.content.DialogInterface
 import android.content.Intent
-import android.graphics.Color
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageButton
-import android.widget.LinearLayout
 import android.widget.TextView
 import android.widget.Toast
-import androidx.appcompat.app.AlertDialog
 import androidx.recyclerview.widget.RecyclerView
 import com.google.firebase.database.FirebaseDatabase
 import com.oneseed.zachet.databinding.FeedItemBinding
