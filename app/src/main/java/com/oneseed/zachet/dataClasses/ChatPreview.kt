dab3fc79f8ee9d202142a2e792853683a98f4cb9
Stepan Nemchenko <st-nemchenko@yandex.ru>
2023-01-21 11:48:16 +0300

    Добавлена документация к ChatPreview и FeedRecord

--- app/src/main/java/com/oneseed/zachet/dataClasses/ChatPreview.kt
+++ app/src/main/java/com/oneseed/zachet/dataClasses/ChatPreview.kt
@@ -1,5 +1,13 @@
 package com.oneseed.zachet.dataClasses
 
+/**
+ * Data-класс элемента чата.
+ *@param receiverName имя и фамилия отправителя
+ *@param latestMsgTime время отправленного сообщения
+ *@param latestMsg текст сообщения
+ *@param isRead индикатор «прочте-ния» сообщения
+ *@param getUser UID получателя
+ *  */
 data class ChatPreview(
     val receiverName: String,
     val latestMsgTime: Long,
--- app/src/main/java/com/oneseed/zachet/dataClasses/FeedRecord.kt
+++ app/src/main/java/com/oneseed/zachet/dataClasses/FeedRecord.kt
@@ -1,5 +1,20 @@
 package com.oneseed.zachet.dataClasses
 
+
+
+
+/**
+ * data-классом элемента записи.
+ *@param author имя и фамилия отправителя
+ *@param authorIdChat UID автора
+ *@param time время публикации
+ *@param record_text текст записи
+ *@param isSponsored индикатор спонсиро-вания записи
+ *@param record уникальный иденти-фикатор записи
+ *@param userId UID пользователя приложения
+ *@param displayLogin логин автора записи
+
+ *  */
 data class FeedRecord(
     val author: String,
     val authorIdChat: String,
